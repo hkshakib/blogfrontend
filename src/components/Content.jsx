79937a047b332c1b292edbaf4b14f2ed1de4388d
hkshakib <humayunkibriashakib@gmail.com>
2023-02-19 00:50:05 +0600

    feat: added API call into content, Added Styles for blog post home page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,125 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Classes from '../styles/Content.module.css';
 
 const Content = () => {
+
+    const [Post, SetPost] = useState(null);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const res = await fetch("http://127.0.0.1:8000/posts/2");
+            const data = await res.json();
+            SetPost(data);
+        };
+
+        fetchApi();
+
+    }, []);
+
+
+    if (Post) {
+        console.log(Post);
+    }
+
+
     return (
-        <div class={Classes.Container}>
-            
+        <div className={Classes.Container}>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>
+            <div className={Classes.PostBox}>
+                <span className={Classes.title}> {Post?.title} </span>
+                <button>View More</button>
+            </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
         </div>
     );
 }
