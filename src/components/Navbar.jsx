b87894655f81046d3b6e1c8daa1a7546b25ccde2
hkshakib <humayunkibriashakib@gmail.com>
2023-02-24 19:58:25 +0600

    feat: Added Custom font and add them into index.html, Navbar Enhancement added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Classes from '../styles/Navbar.module.css';
 
 const Navbar = () => {
 
-    const [category, setCategory] = useState(null);
+    // const [category, setCategory] = useState(null);
 
-    useEffect(() => {
-        const fetchApi = async () => {
-            const res = await fetch("http://127.0.0.1:8000/category");
-            const data = await res.json();
-            setCategory(data);
-        };
+    // useEffect(() => {
+    //     const fetchApi = async () => {
+    //         const res = await fetch("http://127.0.0.1:8000/category");
+    //         const data = await res.json();
+    //         setCategory(data);
+    //     };
 
-        fetchApi();
+    //     fetchApi();
 
-    }, []);
+    // }, []);
 
     return (
         <div className={Classes.FixedNavbar}>
             <div className={Classes.navbar}>
                 <nav>
-                    <a className={Classes.active} href="#">Home</a>
-                    {category?.map((categorys) => {
-                        return (
-                            <div>
-                                <a href='#'>{categorys.name}</a>
-                            </div>
-                        )
-                    })}
-                    <div className={Classes.SignUp}>Beacme A Writer</div>
+                    <div className={Classes.PrimaryClasses}>
+                        <a className={Classes.active} href="#">Bookmarks</a>
+                        <a href='#'>Request a blog</a>
+                    </div>
+                    <div className={Classes.MainClass}>
+                        <span className={Classes.HomeIcon}>MINIMALISTIC</span>
+                    </div>
+                    <div className={Classes.AuthClasses}>
+                        <div className={Classes.SignUp}> <a href='#'> Beacme A Writer </a> </div>
+                        <div className={Classes.Login}> <a href='#'> Login </a> </div>
+                    </div>
                 </nav>
             </div>
         </div>
--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -4,7 +4,9 @@ import Classes from '../styles/RightSideBar.module.css';
 const RightSideBar = ()=> {
   return (
     <div className={Classes.RightSideBar}>
-        
+        <div className={Classes.FeaturePost}>
+          
+        </div>
     </div>
   )
 };
